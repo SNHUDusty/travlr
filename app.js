ba6c858d1c330b6a83cc5550e19f98ebae1fa481
Dusty Cook <dustywyattcook@gmail.com>
2026-05-21 20:48:20 -0700

    Added travel HBS route and partials

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('hbs');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views', 'partials'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
